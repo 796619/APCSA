e4e9f0d675ec5be22b82d088edf23d1e6284ae55
796619 <42595742+796619@users.noreply.github.com>
2018-09-27 10:35:01 -0700

    labcommit

---
+++ Labs/lab(8.22)/Student.java
@@ -0,0 +1,37 @@
+public class Student{
+    private int studNumber;
+    private boolean studActive;
+    private double stuGPA;
+    private String stuName;
+    public Student(){
+        // initialise instance variables
+        studNumber = 0;
+        studActive = false;
+        stuGPA = 0;
+        stuName = "";
+    }
+    public void setStudNumber(int sn)    {
+        studNumber = sn;
+    }
+    public int getStudNumber()    {
+        return studNumber;
+    }
+    public void setStudActive(int sn)    {
+        studNumber = sn;
+    }
+    public int getStudActive()    {
+        return studNumber;
+    }
+    public void setStuGPA(int sn)    {
+        studNumber = sn;
+    }
+    public int getStuGPA()    {
+        return studNumber;
+    }
+    public void setStuName(int sn)    {
+        studNumber = sn;
+    }
+    public int getStuName()    {
+        return studNumber;
+    }
+}
---
+++ Labs/lab(8.22)/StudentRunner.java
@@ -0,0 +1,12 @@
+public class StudentRunner
+{
+    public static void main(){
+        Student s1 = new Student();
+        Student s2 = new Student();
+        s1.setStudNumber(796619);
+        s1.setStuActive(true);
+        s1.setStuGPA(4.0);
+        System.out.println(s1.getStudNumber());
+        System.out.println                                                                                      (s1.getStuActive());
+        System.out.println(s1.getStuGPA());
+}
