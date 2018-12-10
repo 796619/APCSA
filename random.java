87f2741b93469cd425f1700951eee99c7621c6fb
796619 <42595742+796619@users.noreply.github.com>
2018-12-10 10:08:50 -0800

    Add files via upload

---
+++ random.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class random{
+    public static void main(){
+        Random dice = new Random();
+        int number;
+        for(int counter=1; counter<=10; counter++){
+            number = 1+dice.nextInt(6);
+            System.out.println(number + " ");
+        }
+    }
+}    
