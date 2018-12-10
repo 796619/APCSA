50f3590abf1792392fe37e42d72410775320a6d0
796619 <42595742+796619@users.noreply.github.com>
2018-12-10 10:00:15 -0800

    Add files via upload

---
+++ average.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class average{
+    public static void main(){
+        Scanner input = new Scanner(System.in);
+        int total = 0;
+        int grade;
+        int average;
+        int counter = 0;
+        while(counter < 10){
+            grade = input.nextInt();
+            total += grade;
+            counter++;
+        }
+        average = total/10;
+        System.out.println("Your average is " + average);
+    }        
+}    
